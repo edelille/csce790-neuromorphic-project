85e429beedec67b4df85f24a761fc3250e412940
Elizabeth De Lille <hao446@yahoo.com>
2022-03-01 20:56:31 -0500

    with working transcript scraper

--- py-db-populator/main.py
+++ py-db-populator/main.py
@@ -1,10 +1,14 @@
 import os
+import urllib
+import urllib3
+import csv
+import json
+
+import youtube_dl
 from dotenv import load_dotenv
 from pathlib import Path
 from youtube_api import YouTubeDataAPI
 from youtube_transcript_api import YouTubeTranscriptApi
-import youtube_dl
-import urllib
 
 TARGET_CHANNELS = [
     'halfasinteresting'
@@ -52,43 +56,10 @@ def getVideoIDs(filename):
             res.append(x)
     return res
 
-def print_video_infos(video_response):
-    items = video_response.get("items")[0]
-    # get the snippet, statistics & content details from the video response
-    snippet         = items["snippet"]
-    statistics      = items["statistics"]
-    content_details = items["contentDetails"]
-    # get infos from the snippet
-    channel_title = snippet["channelTitle"]
-    title         = snippet["title"]
-    description   = snippet["description"]
-    publish_time  = snippet["publishedAt"]
-    # get stats infos
-    comment_count = statistics["commentCount"]
-    like_count    = statistics["likeCount"]
-    dislike_count = statistics["dislikeCount"]
-    view_count    = statistics["viewCount"]
-    # get duration from content details
-    duration = content_details["duration"]
-    # duration in the form of something like 'PT5H50M15S'
-    # parsing it to be something like '5:50:15'
-    parsed_duration = re.search(f"PT(\d+H)?(\d+M)?(\d+S)", duration).groups()
-    duration_str = ""
-    for d in parsed_duration:
-        if d:
-            duration_str += f"{d[:-1]}:"
-    duration_str = duration_str.strip(":")
-    print(f"""\
-    Title: {title}
-    Description: {description}
-    Channel Title: {channel_title}
-    Publish time: {publish_time}
-    Duration: {duration_str}
-    Number of comments: {comment_count}
-    Number of likes: {like_count}
-    Number of dislikes: {dislike_count}
-    Number of views: {view_count}
-    """)
+class Entry:
+    def __init__(self, video_id, captions):
+        self.video_id = video_id
+        self.captions = captions
 
 def main():
     print("Starting main() function")
@@ -97,39 +68,61 @@ def main():
 
     # get a response via YoutubeTranscriptApi.get_transcript(video id)
     yt = GLOBAL_YT
-    chanFiles = os.listdir(VID_REPO)
+    chanFiles = os.listdir(VID_REPO)[1:]
+
+    # To aggregate the entries
+    entries = []
 
     for chanFile in chanFiles:
         # get videos from each channel
         vids = getVideoIDs(f"{VID_REPO}/{chanFile}")
         print(f"{chanFile}: {len(vids)}")
 
-        for i in range(1):
+        for i in range(len(vids)):
             video_id = vids[i]
 
-            print(f"video id: {video_id}")
-            captions = YouTubeTranscriptApi.get_transcript(video_id)
+            try:
+                captions = YouTubeTranscriptApi.get_transcript(video_id)
+            except:
+                continue
 
-            # grab video metadata
-            part_opts = ['contentDetails', 'fileDetails', 'statistics', 'snippet']
+            entries.append({'vid': video_id, 'transcript': captions})
+            print(f'{chanFile}: ({i+1}/{len(vids)})')
+
+            # grab video metadata TODO some other day
+            '''
+            part_opts = ['contentDetails', 'statistics', 'snippet']
+            print('%2C'.join(part_opts))
             params = {
-                'key': API_KEY,
                 'part': '%2C'.join(part_opts),
                 'id': video_id,
+                'key': API_KEY
             }
             uri = urllib.parse.urlencode(params)
             full_url = YT_VIDEOS_ENDPOINT + uri
 
-            # Get the response
+            # Get the response from the API
+            print(full_url)
+            http = urllib3.PoolManager()
+            r = http.request('GET', full_url)
 
+            print(r.data)
+            '''
 
-        print("\n")
+    print(f'Channels completed parsing; {len(entries)} videos were parsed')
+    # Work with csv now to output
+    with open('output/vid_captions.csv', 'w') as f:
+        writer = csv.writer(f)
 
+        for entry in entries:
+            # encode the captions
+            # substep, convert time/caption pairs into a single string
+            captions = []
+            for caption in entry['transcript']:
+               captions.append(json.dumps(caption)) 
 
-
-
-
-    # Work with each caption in response
+            encodedCaptions = f"[{','.join(captions)}]"
+            writer.writerow([entry['vid'], encodedCaptions])
 
 
 if __name__ == '__init__':
